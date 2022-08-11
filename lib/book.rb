95b8f0d7a681d7fb63a07250fbc65640d92e349c
Abzed <ibraabzed@gmail.com>
2022-08-11 18:05:46 +0300

    commit tree 5d652a12e50c586471784576ef26ca3cf73e9c2e

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,26 @@
+class Book
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+    def initialize(title="And Then There Were None")
+        @title = title
+    end
 
+    def initialize(author)
+        @author = author
+    end
+
+    def initialize(page_count)
+        @page_count = page_count
+    end
+
+    def initialize(genre)
+        @genre = genre
+    end
+
+    def turn_page
+        print "Flipping the page...wow, you read fast!"
+    end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,37 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+    def initialize(brand="Adidas")
+        @brand = brand
+    end
+
+    def initialize(color)
+        @color = color
+    end
+
+    def initialize(size)
+        @size = size
+    end
+
+    def initialize(material)
+        @material = material
+    end
+
+    def initialize(condition)
+        @condition = condition
+    end
+
+    def cobble
+        def condition(new_condition="new")
+            @condition = new_condition
+        end
+        # shoe = Shoe.new
+        # shoe.condition = "new"
+        print "Your shoe is as good as new!"
+    end
+end
+
